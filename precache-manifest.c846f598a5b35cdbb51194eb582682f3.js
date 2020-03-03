45ae625010d4140133540fb114498f9f27b92be4
belousoWork <belousowork@gmail.com>
2020-03-03 23:49:06 +0200

    Updates

---
+++ precache-manifest.c846f598a5b35cdbb51194eb582682f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48a52a62ee862a1714d96fac2f3e453a",
+    "url": "/mlpa/index.html"
+  },
+  {
+    "revision": "72be3d8e21bae67a5dfe",
+    "url": "/mlpa/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "0ab6b7dd645bcf2e91ed",
+    "url": "/mlpa/static/js/2.3a153f2c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/mlpa/static/js/2.3a153f2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72be3d8e21bae67a5dfe",
+    "url": "/mlpa/static/js/main.f283078a.chunk.js"
+  },
+  {
+    "revision": "284538f68ea1729733dc",
+    "url": "/mlpa/static/js/runtime-main.7c12251b.js"
+  }
+]);
