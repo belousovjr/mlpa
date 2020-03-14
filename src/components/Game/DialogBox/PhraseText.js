a98ac1dc29b91d8a3d2c205ef18c1efd5b08091a
mr.williams.org <mr.williams.org@gmail.com>
2020-03-14 11:38:13 +0200

    added ballanse text

--- src/components/Game/DialogBox/PhraseText.js
+++ src/components/Game/DialogBox/PhraseText.js
@@ -4,7 +4,7 @@ import "./style.css";
 export default class PhraseText extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { isStart: true, curText: "" };
+    this.state = { isStart: true, curText: "", balText: "" };
   }
   nextLetterLoop() {
     const { text, pAnsswerd, writingFinish, isPlayer } = this.props;
@@ -14,21 +14,24 @@ export default class PhraseText extends React.Component {
       const curLen = curText.length;
       const newText = text.slice(0, curLen + 1);
 
+      const balText = text.slice(curLen + 1);
+
       setTimeout(() => {
-        this.setState({ curText: newText, isStart: false });
+        this.setState({ curText: newText, isStart: false, balText });
         this.nextLetterLoop();
       }, 40);
     } else isPlayer ? pAnsswerd() : writingFinish();
   }
   render() {
     const { isPlayer } = this.props;
-    const { isStart, curText } = this.state;
+    const { isStart, curText, balText } = this.state;
     if (isStart) this.nextLetterLoop();
     const title = isPlayer ? "Я" : "Она";
     const titleClass = isPlayer ? "text-p" : "text-c";
     return (
       <div className="phrase-text">
         <span className={titleClass}>{title}/</span> {curText}
+        <span className="bal-text">{balText}</span>
       </div>
     );
   }
